be0123bd840d1995a6b21ca1b2c04bc55498bd1b
Noah <ngregrichardson@gmail.com>
2020-03-28 11:29:21 -0400

    feat: Update build

---
+++ client/build/precache-manifest.e9ffc26ddc0c2dae14a6c143e447abd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "258fff82daa718b42bf51cf2011c25fc",
+    url: "/index.html",
+  },
+  {
+    revision: "c0359edbf740d0f0c6fe",
+    url: "/static/css/2.47e06e2e.chunk.css",
+  },
+  {
+    revision: "7830b103c2ed35b3a7a1",
+    url: "/static/css/main.f21bd0c9.chunk.css",
+  },
+  {
+    revision: "c0359edbf740d0f0c6fe",
+    url: "/static/js/2.501b5b4f.chunk.js",
+  },
+  {
+    revision: "90fca578673fd01f7c02049d3d4d9dfb",
+    url: "/static/js/2.501b5b4f.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "7830b103c2ed35b3a7a1",
+    url: "/static/js/main.2c846d1d.chunk.js",
+  },
+  {
+    revision: "156009f602b5b675ad9e",
+    url: "/static/js/runtime-main.56c03351.js",
+  },
+]);
