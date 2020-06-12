d7497108a276ea198085959268ef64752c5a3d61
Noah <ngregrichardson@gmail.com>
2020-06-11 23:54:34 -0400

    chore: Update port

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const fetch = require("node-fetch");
 const moment = require("moment");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6000;
 const app = express();
 app.use(express.json());
 app.use(cors());
